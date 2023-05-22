cb1af07a6fa6f68f0db180983289b57d37c94486
Christian Valeri <mortlaxris@outlook.com>
2023-05-22 18:45:19 -0500

    .

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const form = document.getElementById("contact");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const phoneNumber = document.getElementById("phone-number");
-
-form.addEventListener("click", (e)=>{
-    e.preventDefault();
-    formValidation();
-});
-
-function formValidation(){
+document.addEventListener("DOMContentLoaded", function() {
+        const form = document.getElementById("contact");
+        const username = document.getElementById("username");
+        const email = document.getElementById("email");
+        const phoneNumber = document.getElementById("phone-number");
     
-}
+        form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        formValidation();
+        });
+    
+        function formValidation() {
+        const usernameInput = username.value.trim();
+    
+        if (usernameInput === "" || usernameInput === null) {
+            
+                console.log("no worky");
+        }
+    
+        // if (emailInput == ' ' || emailInput == null){
+        // }
+    
+        // if (phoneNumberInput == ' ' || phoneNumberInput == null){
+        // }
+        }
+    });
+    
