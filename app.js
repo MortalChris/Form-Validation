d7b8d1d50157b5173035cf1b3ad21c7fc501876b
Christian Valeri <mortlaxris@outlook.com>
2023-05-22 18:15:00 -0500

    start

---
+++ app.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("contact");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const phoneNumber = document.getElementById("phone-number");
+
+form.addEventListener("click", (e)=>{
+    e.preventDefault();
+    formValidation();
+});
+
+function formValidation(){
+    
+}
