105f1d1837f174732cd4eebad06b0254d9ca4dfa
Christian Valeri <mortlaxris@outlook.com>
2023-05-23 17:03:09 -0500

    seterror, setsuccess,removeSetError functions

--- app.js
+++ app.js
@@ -3,25 +3,62 @@ document.addEventListener("DOMContentLoaded", function() {
         const username = document.getElementById("username");
         const email = document.getElementById("email");
         const phoneNumber = document.getElementById("phone-number");
-    
+
         form.addEventListener("submit", function(e) {
-        e.preventDefault();
-        formValidation();
-        });
-    
-        function formValidation() {
-        const usernameInput = username.value.trim();
-    
-        if (usernameInput === "" || usernameInput === null) {
+                e.preventDefault();
                 
-                console.log("no test");
+                formValidation();
+        });
+
+        function formValidation() {
+                const usernameInput = username.value.trim();
+                        if (usernameInput === "" || usernameInput === null) {
+                        setError(username);
+                        }else{
+                                setSuccess(username);
+                        }
+
+                const emailInput = email.value.trim();
+                        if (emailInput === "" || emailInput === null) {
+                        setError(email);
+                        }else{
+                                setSuccess(email);
+                        }
+
+                const phoneNumberInput = phoneNumber.value.trim();
+                        if (phoneNumberInput === "" || phoneNumberInput === null) {
+                        setError(phoneNumber);
+                        } else{
+                                setSuccess(phoneNumber);
+                        }
+                }
+
+
+
+        function setError(element){
+                removesetError(element);
+        
+                const pErrorText = document.createElement("p");
+                        pErrorText.className = "pErrorText";
+                        pErrorText.textContent = element.id + " is not entered!";
+                        pErrorText.style.color = "red";
+                        element.style.borderColor = "red";
+                const parentElement = element.parentElement;
+                        parentElement.appendChild(pErrorText);
         }
-    
-        // if (emailInput == ' ' || emailInput == null){
-        // }
-    
-        // if (phoneNumberInput == ' ' || phoneNumberInput == null){
-        // }
+
+        function setSuccess(element){
+                removesetError();
+                console.log("im being called");
+                element.style.borderColor = "green";
         }
-    });
-    
+
+        function removesetError(element){
+                const currentsetError = element.parentElement.querySelectorAll(".pErrorText");
+                for(let i = 0; i < currentsetError.length; i++){
+                        currentsetError[i].remove();
+                }
+        }
+
+
+});
